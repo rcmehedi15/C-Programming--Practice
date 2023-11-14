959b125e749ee4ea6bff8f4acbfd5761993e1fe5
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-11-14 21:07:42 +0600

    switch

---
+++ Class-2/condition_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x =10;
+    if(x=10)
+    {
+        cout<<"Its 10"<<endl;
+    }
+    else{
+        "Its not 10";
+    }
+   
+}
---
+++ Class-2/switch.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x ;
+    cin>>x;
+    switch(x)
+    {
+        case 1:
+            cout<<"1" <<endl;
+            break;
+        case 2:
+            cout<<"2" <<endl;
+            break;
+        case 3:
+            cout<<"3" <<endl;
+            break;
+        case 4:
+            cout<<"4" <<endl;
+            break;
+        default:
+            cout<<"No";
+    }
+    // if (x == 1)
+    // {
+    //     cout << "1";
+    // }
+    // else if (x == 2)
+    // {
+    //     cout << "2";
+    // }
+    // else if (x == 3)
+    // {
+    //     cout << "3";
+    // }
+    // else if (x == 4)
+    // {
+    //     cout << "4";
+    // }
+    // else if (x == 5)
+    // {
+    //     cout << "5";
+    // }
+}
