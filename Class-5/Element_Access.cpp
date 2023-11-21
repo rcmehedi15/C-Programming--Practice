fc717807dba19a812be248b0ef80a67e75052502
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-11-21 23:00:29 +0600

    element access

---
+++ Class-5/Element_Access.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>> s;
+    // cout<<s[7]<<endl;
+    cout<<s[s.size()-1]<<endl; // use
+    cout<<s.at(0)<<endl;
+    cout<<s.back()<<endl; // use
+    cout<<s.front()<<endl;
+    return 0;
+}
